779da91ecc455d568c39072ac91c9f35f96708ca
ashan <uhashan17929@gmail.com>
2024-06-26 12:48:54 +0530

    Project Update

---
+++ src/Components/Education.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className='p-4 mx-auto bg-[#0c0c0c] bg-center bg-cover'>
+      <div className=" ">
+        <h2 className='p-8 font-serif text-6xl md-text-8xl text-white transform hover:text-[#f7bb7e]'>Education</h2>
+      </div>
+
+      <div className="flex flex-col justify-between gap-2 md:flex-row">
+            <div className="w-full p-4 text-white md:w-1/2">
+                <div className=' p-8 mb-6 bg-black rounded-2xl hover:bg-gradient-to-r from-[#382753] to-[#170c2b] font-bold'>
+                    <h3 className='text-2xl font-bold text-[#8750f7] uppercase'>2022 Present</h3>
+                    <p className='text-3xl uppercase'>Bsc (Hons). in Computing</p>
+                    <p className='text-xl font-semibold'>IDM Nation Campus</p>
+                    <p>Successfull Complete</p>
+                </div>
+
+                <div className=' p-8 mb-6 bg-black rounded-2xl hover:bg-gradient-to-r from-[#382753] to-[#170c2b] font-bold'>
+                    <h3 className='text-2xl font-bold text-[#8750f7] uppercase'>2022 Present</h3>
+                    <p className='text-3xl uppercase'>Bsc (Hons). in Computing</p>
+                    <p className='text-xl font-semibold'>IDM Nation Campus</p>
+                    <p>Successfull Complete</p>
+                </div>
+
+                <div className=' p-8 mb-6 bg-black rounded-2xl hover:bg-gradient-to-r from-[#382753] to-[#170c2b] font-bold'>
+                    <h3 className='text-2xl font-bold text-[#8750f7] uppercase'>2022 Present</h3>
+                    <p className='text-3xl uppercase'>Bsc (Hons). in Computing</p>
+                    <p className='text-xl font-semibold'>IDM Nation Campus</p>
+                    <p>Successfull Complete</p>
+                </div>
+            </div>
+
+            <div className='w-full p-4 text-white md:w-1/2'>
+                
+                <div className=' p-8 mb-6 bg-black rounded-2xl hover:bg-gradient-to-r from-[#382753] to-[#170c2b] font-bold'>
+                    <h3 className='text-2xl font-bold text-[#8750f7] uppercase'>2022 Present</h3>
+                    <p className='text-3xl uppercase'>Bsc (Hons). in Computing</p>
+                    <p className='text-xl font-semibold'>IDM Nation Campus</p>
+                    <p>Successfull Complete</p>
+                </div>
+
+                <div className=' p-8 mb-6 bg-black rounded-2xl hover:bg-gradient-to-r from-[#382753] to-[#170c2b] font-bold'>
+                    <h3 className='text-2xl font-bold text-[#8750f7] uppercase'>2022 Present</h3>
+                    <p className='text-3xl uppercase'>Bsc (Hons). in Computing</p>
+                    <p className='text-xl font-semibold'>IDM Nation Campus</p>
+                    <p>Successfull Complete</p>
+                </div>
+
+                <div className=' p-8 mb-6 bg-black rounded-2xl hover:bg-gradient-to-r from-[#382753] to-[#170c2b] font-bold'>
+                    <h3 className='text-2xl font-bold text-[#8750f7] uppercase'>2022 Present</h3>
+                    <p className='text-3xl uppercase'>Bsc (Hons). in Computing</p>
+                    <p className='text-xl font-semibold'>IDM Nation Campus</p>
+                    <p>Successfull Complete</p>
+                </div>
+
+            </div>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Education
